a963f42b009cf74855d3522fe51ad5e0ee7b1184
Natalia Mamunina <mamunina-nv@roxiemobile.com>
2019-09-03 16:45:57 +0300

    Add tests to LFArchiver.

---
+++ Modules/RoxieMobile.SwiftCommons/Tests/Data.UnitTests/Sources/StreamTypedCoder/StreamTypedCoderTests.MixedModelTests.swift
@@ -0,0 +1,104 @@
+// ----------------------------------------------------------------------------
+//
+//  StreamTypedCoderTestsMixedModelTests.swift
+//
+//  @author     Natalia Mamunina <mamunina-nv@roxiemobile.com>
+//  @copyright  Copyright (c) 2019, Roxie Mobile Ltd. All rights reserved.
+//  @link       http://www.roxiemobile.ru/
+//
+// ----------------------------------------------------------------------------
+
+@testable import SwiftCommonsObjC
+import XCTest
+
+// ----------------------------------------------------------------------------
+
+class StreamTypedCoderTestsMixedModelTests: StreamTypeCoderTests
+{
+// MARK: - Tests
+
+    func testMixedModel() {
+        let _mixObject: STCMixedModel? = STCMixedModel.shared
+
+        // Positive
+        assertNoThrow {
+            // Encode
+            let data = NSMutableData()
+            StreamTypedEncoder(forWritingWith: data).encodeRootObject(_mixObject)
+            XCTAssertNotEqual(data, NSMutableData())
+
+            print("debug: LFArchiver: \(data.length)")
+
+            // Decode
+            var _mixResult: STCMixedModel?
+            if let value = StreamTypedDecoder(forReadingWith: data as Data)?.decodeObject() as? STCMixedModel {
+                _mixResult = value
+            }
+            XCTAssertEqual(_mixObject, _mixResult)
+        }
+    }
+
+    func testMixedModel_ToStringJSON() {
+        let _mixObject: STCMixedModel? = STCMixedModel.shared
+
+        // Positive
+        assertNoThrow {
+            // Encode
+            let jsonString = _mixObject?.toJSONString()
+
+            print("debug: JSONString: \(jsonString?.lengthOfBytes(using: .utf8))")
+
+            // Decode
+            var _mixResult: STCMixedModel?
+            if let string = jsonString {
+                _mixResult = try? STCMixedModel(from: string)
+            }
+            XCTAssertEqual(_mixObject, _mixResult)
+        }
+    }
+
+    func testSpeed_MixedModel() {
+        let _mixObject: STCMixedModel? = STCMixedModel.shared
+
+        // Positive
+        assertNoThrow {
+            var i = 0
+            while i < 1000 {
+                // Encode
+                let data = NSMutableData()
+                StreamTypedEncoder(forWritingWith: data).encodeRootObject(_mixObject)
+                XCTAssertNotEqual(data, NSMutableData())
+
+                // Decode
+                var _mixResult: STCMixedModel?
+                if let value = StreamTypedDecoder(forReadingWith: data as Data)?.decodeObject() as? STCMixedModel {
+                    _mixResult = value
+                }
+                XCTAssertEqual(_mixObject, _mixResult)
+                i += 1
+            }
+        }
+    }
+
+    func testSpeed_MixedModel_ToStringJSON() {
+        let _mixObject: STCMixedModel? = STCMixedModel.shared
+
+        // Positive
+        assertNoThrow {
+            var i = 0
+            while i < 1000 {
+                // Encode
+                let jsonString = _mixObject?.toJSONString()
+
+                // Decode
+                var _mixResult: STCMixedModel?
+                if let string = jsonString {
+                    _mixResult = try? STCMixedModel(from: string)
+                }
+
+                XCTAssertEqual(_mixObject, _mixResult)
+                i += 1
+            }
+        }
+    }
+}
